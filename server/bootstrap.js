368d035df6c983c0a87861daa18aab442174f510
Kemal Hrelja <mail@kemoke.net>
2024-05-09 17:00:34 +0200

    sign file url

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const { getAbsoluteServerUrl } = require('@strapi/utils');
+const {getAbsoluteServerUrl} = require('@strapi/utils');
 
-module.exports = ({ strapi }) => {
+module.exports = ({strapi}) => {
   const generateBlurhash = async (event, eventType) => {
-    const { data, where } = event.params;
+    const {data, where} = event.params;
 
     if ((data.mime && data.mime.startsWith('image/'))) {
       let fullUrl = "";
-      if (data.url.startsWith('http')) {
-        fullUrl = data.url;
+      const signedFile = await strapi
+        .plugin("upload")
+        .service("file")
+        .signFileUrls(data);
+      if (signedFile.url.startsWith('http')) {
+        fullUrl = signedFile.url;
       } else {
-        fullUrl = `${getAbsoluteServerUrl(strapi.config)}${data.url}`;
+        fullUrl = `${getAbsoluteServerUrl(strapi.config)}${signedFile.url}`;
       }
       data.blurhash = await strapi.plugin('strapi-blurhash').service('blurhash').generateBlurhash(fullUrl);
     }
@@ -19,22 +23,26 @@ module.exports = ({ strapi }) => {
     if (eventType === 'beforeUpdate') {
       const regenerateOnUpdate = strapi.plugin('strapi-blurhash').config('regenerateOnUpdate');
       const forceRegenerateOnUpdate = strapi.plugin('strapi-blurhash').config('forceRegenerateOnUpdate');
-    
+
       const fullData = await strapi.db.query('plugin::upload.file').findOne({
         select: ['url', 'blurhash', 'name', 'mime'],
         where
       });
-    
+
       if (fullData.mime.startsWith('image/') && (forceRegenerateOnUpdate || (!fullData.blurhash && regenerateOnUpdate))) {
         let fullDataUrl = "";
-        if (fullData.url.startsWith('http')) {
-          fullDataUrl = fullData.url;
+        const signedFile = await strapi
+          .plugin("upload")
+          .service("file")
+          .signFileUrls(data);
+        if (signedFile.url.startsWith('http')) {
+          fullDataUrl = signedFile.url;
         } else {
-          fullDataUrl = `${getAbsoluteServerUrl(strapi.config)}${fullData.url}`;
+          fullDataUrl = `${getAbsoluteServerUrl(strapi.config)}${signedFile.url}`;
         }
         data.blurhash = await strapi.plugin('strapi-blurhash').service('blurhash').generateBlurhash(fullDataUrl);
       }
-    }    
+    }
   };
 
   strapi.db.lifecycles.subscribe({
